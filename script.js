6c7ba1920cd7f24f6290cef3219bf119a5711032
chenberyl <77847595+chenberyl@users.noreply.github.com>
2021-04-22 23:46:05 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  var x = document.getElementById("age").value;
+  document.getElementById("text-output").innerHTML = x*7;
+}
+
+// inspired by https://codepen.io/tommy83/pen/PoobKPg
